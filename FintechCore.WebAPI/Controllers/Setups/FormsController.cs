404d885da460dc7bac8c53408c29c380b9bf47d0
Ahercode <philipkelly407@gmail.com>
2025-10-26 07:21:29 +0000

    completed the form controller

--- FintechCore.WebAPI/Controllers/Setups/FormsController.cs
+++ FintechCore.WebAPI/Controllers/Setups/FormsController.cs
@@ -1,3 +1,4 @@
+using FintechCore.Application.Dtos.Setups;
 using FintechCore.Application.Services.Setups.form;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,5 +13,45 @@ public class FormsController: ControllerBase
     public FormsController(IFormService formService)
     {
         _formService = formService;
+    }
+    
+    // get all forms
+    [HttpGet]
+    public async Task<IActionResult> GetAllForms()
+    {
+        var forms = await _formService.GetAllFormesAsync();
+        return Ok(forms);
+    }
+    
+    // get form by id
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetFormById(Guid id)
+    {
+        var form = await _formService.GetFormByIdAsync(id);
+        return Ok(form);
+    }
+    
+    // create form
+    [HttpPost]
+    public async Task<IActionResult> CreateForm([FromBody] CreateFormDto dto )
+    {
+        var form = await _formService.CreateFormAsync(dto);
+        return CreatedAtAction(nameof(GetFormById), new { id = form.FormId }, form);
+    }
+    
+    // update form
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateForm(Guid id, [FromBody] UpdateFormDto dto)
+    {
+        var form = await _formService.UpdateFormAsync(id, dto);
+        return Ok(form);
+    }
+    
+    // delete form
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteForm(Guid id)
+    {
+        await _formService.DeleteFormAsync(id);
+        return Ok("Form deleted successfully");
     }
 }
