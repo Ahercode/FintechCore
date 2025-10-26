d645bab133e54d25b5d7df8176718b8f7e7926e0
Ahercode <philipkelly407@gmail.com>
2025-10-26 15:35:45 +0000

    Complete lovController and formController

--- FintechCore.WebAPI/Controllers/Setups/LovsController.cs
+++ FintechCore.WebAPI/Controllers/Setups/LovsController.cs
@@ -1,3 +1,4 @@
+using FintechCore.Application.Dtos.Setups;
 using FintechCore.Application.Services.Setups.lov;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,5 +13,36 @@ public class LovsController : ControllerBase
     public LovsController(ILovService lovService)
     {
         _lovService = lovService;
+    }
+    
+    // get all lovs
+    [HttpGet]
+    public async Task<IActionResult> GetAllLovs()
+    {
+        var lovs = await _lovService.GetAllLovsAsync();
+        return Ok(lovs);
+    }
+    
+    // get lov by id
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetLovById(Guid id)
+    {
+        var lov = await _lovService.GetLovByIdAsync(id);
+        return Ok(lov);
+    }
+    
+    // create lov
+    [HttpPost]
+    public async Task<IActionResult> CreateLov([FromBody] CreateLovDto dto)
+    {
+        var lov = await _lovService.CreateLovAsync(dto);
+        return Ok(lov);
+    }
+    
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteLov(Guid id)
+    {
+        await _lovService.DeleteLovAsync(id);
+        return Ok("Lov deleted successfully");
     }
 }
